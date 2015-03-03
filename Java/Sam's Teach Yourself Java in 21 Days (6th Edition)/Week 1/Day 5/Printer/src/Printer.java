e77f3fa1565b4665d058d0676e8572b430a4ca41
Justin <jrubin@local.host>
2015-03-03 18:15:36 -0500

    Finished day 5
    finished day 5

---
+++ Java/Sam's Teach Yourself Java in 21 Days (6th Edition)/Week 1/Day 5/Printer/src/Printer.java
@@ -0,0 +1,23 @@
+class Printer {
+	int x = 0;
+	int y = 1;
+	
+	void printMe() {
+		System.out.println("x is " + x + ", y is " + y);
+		System.out.println("I am an instance of the class " + this.getClass().getName());
+	}
+}
+
+class SubPrinter extends Printer {
+	int z = 3;
+	
+	void printMe() {
+		System.out.println("x is " + x + ", y is " + ", z is " + z);
+		System.out.println("I am an instance of the class " + this.getClass().getName());
+	}
+	
+	public static void main(String[] args) {
+		SubPrinter obj = new SubPrinter();
+		obj.printMe();
+	}
+}
